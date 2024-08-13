b42911063c3e13ffe55f571afb6a0e683214d15c
diljith krishna <diljithkrishna56@gmail.com>
2024-08-13 21:19:38 +0530

    responsive issue solving

--- src/components/User/About.jsx
+++ src/components/User/About.jsx
@@ -8,19 +8,19 @@ const About = () => {
             <motion.img
                 src={MyImg}
                 alt="Image"
-                className="sm:w-[20rem] w-[15rem] rounded-full sm:ml-[6rem] ml-0 sm:mt-0 mt-10 shadow-lg"
+                className="w-[15rem] sm:w-[20rem] rounded-full shadow-lg sm:mr-6 mb-4 sm:mb-0" // Adjust margins and widths for responsiveness
                 initial={{ opacity: 0, scale: 0.8 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ duration: 0.8 }}
             />
-            <div className="flex flex-col sm:ml-[8rem] sm:items-start items-center">
+            <div className="flex flex-col items-center sm:items-start sm:ml-6">
                 <motion.div
                     className="p-3"
                     initial={{ opacity: 0, x: -50 }}
                     animate={{ opacity: 1, x: 0 }}
                     transition={{ duration: 1 }}
                 >
-                    <h1 className="text-xl mb-2 text-[#34d399] font-extrabold text-center sm:text-left">MERN STACK DEVELOPER</h1>
+                    <h1 className="text-xl sm:text-2xl mb-2 text-[#34d399] font-extrabold text-center sm:text-left">MERN STACK DEVELOPER</h1>
                     <div className="border border-[#aaaaaa] rounded-xl p-6 bg-[#09090b] shadow-lg">
                         <p className="text-[#aaaaaa] font-medium font-sans text-center sm:text-left">
                             Hey there! I'm Diljith Krishna, a dynamic force in the world of web development,
@@ -30,10 +30,10 @@ const About = () => {
                         </p>
                     </div>
                 </motion.div>
-                <div className="sm:ml-[1rem] pt-8 max-w-full w-auto">
+                <div className="pt-8 max-w-full w-auto">
                     <div className="border-b border-[#aaaaaa] opacity-50 w-full"></div>
                     <motion.div
-                        className="flex items-center gap-2 mt-1 ml-0 sm:ml-0 p-4"
+                        className="flex items-center gap-2 mt-1 p-4"
                         initial={{ opacity: 0, x: 50 }}
                         animate={{ opacity: 1, x: 0 }}
                         transition={{ duration: 1 }}
@@ -44,6 +44,7 @@ const About = () => {
                 </div>
             </div>
         </div>
+
     );
 };
 
